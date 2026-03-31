3179e07a7685c1da6d28fff389679fea3c36041b
alexj <alexjvd@outlook.com>
2026-03-31 16:16:21 -0400

    Added comments and more verified input cases

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ def max_val_substring(character_values, a, b):
 
     memory = [[0] * (m + 1) for _ in range(n + 1)]
 
+    # Fill Memory 2d array
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             if a[i - 1] == b[j - 1]:
@@ -68,6 +69,7 @@ def max_val_substring(character_values, a, b):
             else:
                 memory[i][j] = max(memory[i - 1][j], memory[i][j - 1])
 
+    # Backtracking to obtain substring
     i, j = n, m
     result = []
     while i > 0 and j > 0:
@@ -89,7 +91,10 @@ def main():
     # Input handling
     filename = input("Type the name of the txt file to use as input\n")
     character_values, a, b = read_input_file(filename)
+
     value, substring = max_val_substring(character_values, a, b)
+
+    # Output handling
     print(value)
     print(substring)
 
